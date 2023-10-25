438cbb04b5c8256825d952beea955221a9147bdd
Ben-Eze Chidiebere <benezchidi@gmail.com>
2023-10-25 19:18:10 +0100

    new iles aded

--- 3. logistic regression/predict.py
+++ 3. logistic regression/predict.py
@@ -3,6 +3,8 @@
 import pickle
 import sklearn
 from flask import Flask
+from flask import request
+from flask import jsonify
 
 model_data = 'model_C1.bin'
 
@@ -13,10 +15,18 @@ app = Flask('churn')
 
 @app.route('/predict', methods=['POST'])
 
-def predict (customer):
+def predict ():
+    customer = request.get_json()
+
     X = dv.transform([customer])
     y_pred = model.predict_proba(X)[0,1]
-    return y_pred
+    churn = y_pred >= 0.5
+
+    result = {
+        'churn probability': float(y_pred),
+        'churn': bool(churn)
+    }
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=9696)
